2776367eac626c00990bb40e6fb3b4652024283b
Renan Parohinog <renanparohinog17@gmail.com>
2022-04-05 21:47:03 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+// Tanggala niyo lang kat comment kat daywang ka makaea hu //
+
+// Number One
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
+
+
+// const num = num1 / num2;
+// const n = num.toFixed(2);
+
+// alert(`The mechanical advantage of ${num1} and ${num2} is ${n}`);
+
+
+
+
+
+
+// Number Two
+// const num1 = parseInt(prompt('Enter the first number '));
+// const num2 = parseInt(prompt('Enter the second number '));
+
+// const output = num1 / num2 * 100;
+
+// alert(`The mechanical effeciency of ${num1} and ${num2} is ${output}%`);
